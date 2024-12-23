d29dfd5ea54e7cf4676f935cfd69a3592c796520
Systemiq0609 <985973@mail.ru>
2024-12-24 00:17:47 +0300

    Add files via upload

---
+++ Greetings.py
@@ -0,0 +1,12 @@
+user_name="Nikita"
+title="Задачи на месяц"
+Content = "Выполнить задачи в течении месяца"
+Status = "В работе"
+Created_date = "03-09-2024"
+Issue_date = "03-10-2024"
+print("имя пользователя:",user_name)
+print("Заголовок заметки:",title)
+print("Описание заметки:",Content)
+print("Заголовок заметки:",Status)
+print("Дата создания заметки:",Created_date )
+print("Дата истечения заметки",Issue_date)
